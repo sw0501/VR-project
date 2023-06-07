93320404d42a7e3e161c1dcb60a5d3a1e3b706c7
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-06-07 17:55:09 +0900

    Feat: Start Scene 에어컨 관련 파티클 기능 추가

--- VR Project/Assets/script/StartSceneInteraction.cs
+++ VR Project/Assets/script/StartSceneInteraction.cs
@@ -6,6 +6,26 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class StartSceneInteraction : MonoBehaviour
 {
+
+    public GameObject particle1;
+    public GameObject particle2;
+    public GameObject particle3;
+    public GameObject fadeout;
+
+
+    IEnumerator Aircondition()
+    {
+        particle1.SetActive(true); 
+        particle2.SetActive(true);
+        particle3.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+
+        fadeout.SetActive(true);
+        fadeout.GetComponent<FadeOut>().FadeOutFunc(1);
+
+    }
+
     public AudioManager audioManager;
 
     public void Awake()
@@ -25,5 +45,10 @@ public class StartSceneInteraction : MonoBehaviour
         XRController right_controller = (XRController)GameObject.FindObjectsOfType(typeof(XRController))[1];
         left_controller.GetComponent<XRController>().SendHapticImpulse(0.7f, duration);
         right_controller.GetComponent<XRController>().SendHapticImpulse(0.7f, duration);
+    }
+
+    public void ActivateAirCondition()
+    {
+        StartCoroutine(Aircondition());
     }
 }
